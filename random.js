025b1efe3843e409a1b501c7b7e6c83503ef7d24
vulong <vulongpt@gmail.com>
2022-01-21 20:41:38 +0700

    super fast server

---
+++ random.js
@@ -0,0 +1,45 @@
+const faker = require("@faker-js/faker");
+const fs = require("fs");
+//locale
+faker.locale = "vi";
+
+function randomListSetName(maxSetName) {
+    if (maxSetName <= 0) return [];
+    const listSetName = [];
+    const randomSetName = Math.ceil(Math.random() * maxSetName);
+    for (let i = 1; i <= randomSetName; i++) {
+        listSetName.push(faker.animal.dog());
+    }
+
+    return listSetName;
+}
+function randomRestaurant(numbersOfRestaurant) {
+    if (numbersOfRestaurant <= 0) return [];
+    const listRestaurant = [];
+    Array.from(new Array(numbersOfRestaurant)).forEach(() => {
+        const restaurant = {
+            restaurantId: faker.datatype.uuid(),
+            restaurantName: faker.name.lastName("female"),
+            phoneNumber: faker.phone.phoneNumber(),
+            status: faker.datatype.boolean(),
+            address: faker.address.cityName(),
+            setName: randomListSetName(5),
+        };
+
+        listRestaurant.push(restaurant);
+    });
+
+    return listRestaurant;
+}
+
+(() => {
+    const listRestaurant = randomRestaurant(40);
+    // prepare data
+    const db = {
+        restaurants: listRestaurant,
+    };
+    // write to db.json
+    fs.writeFile("./db.json", JSON.stringify(db), () => {
+        console.log("Write Successfully =))");
+    });
+})();
---
+++ readme.md
@@ -0,0 +1,12 @@
+You can run:
+
+# npm start
+
+# yarn start
+
+-   Main.js:
+    Cấu hình router cho json server.
+    Tự động thêm createdAt và updatedAt vào trong req.body
+-   random.js
+    Dùng "@faker-js/faker" để random dữ liệu
+    Dùng fs system built in nodejs để đọc và ghi file vào trong db.json
