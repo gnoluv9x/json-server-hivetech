eef4348564805296b28bb690c39df6e9786d4713
vulong <vulongpt@gmail.com>
2022-01-24 01:29:45 +0700

    update authentication

--- index.js
+++ index.js
@@ -118,6 +118,8 @@ server.post("/auth/register", (req, res) => {
 // You can use the one used by JSON Server
 
 server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === "POST") {
         req.body.createdAt = Date.now();
         req.body.updatedAt = Date.now();
