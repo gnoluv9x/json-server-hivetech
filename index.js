e380e2f29d32f32429d135434283cabf20297e0d
manhkien198 <manhkien1998@gmail.com>
2022-01-24 00:27:10 +0700

    commit on master

--- index.js
+++ index.js
@@ -24,74 +24,85 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 function createToken(payload) {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn });
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
 // Verify the token
 function verifyToken(token) {
-    return jwt.verify(token, SECRET_KEY, (err, decode) => (decode !== undefined ? decode : err));
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
 }
 
 // Check if the user exists in database
 function isAuthenticated({ email, password }) {
-    const userdb = JSON.parse(fs.readFileSync("./db.json", "UTF-8"));
-    return (
-        userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
-    );
+  const userdb = JSON.parse(fs.readFileSync("./db.json", "UTF-8"));
+  return (
+    userdb.users.findIndex(
+      (user) => user.email === email && user.password === password
+    ) !== -1
+  );
 }
 
 server.post("/auth/login", (req, res) => {
-    const { email, password } = req.body;
-    if (isAuthenticated({ email, password }) === false) {
-        const status = 401;
-        const message = "Incorrect email or password";
-        res.status(status).json({ status, message });
-        return;
-    } else {
-        // get userID
-        const userInfo = userdb.users.find(item => item.email === email);
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401;
+    const message = "Incorrect email or password";
+    res.status(status).json({ status, message });
+    return;
+  } else {
+    // get userID
+    const userInfo = userdb.users.find((item) => item.email === email);
 
-        if (userInfo) {
-            const { email, password, id } = userInfo;
-            const bytes = utf8.encode(password);
-            const encoded = base64.encode(bytes);
-            const access_token = createToken({ email, password });
-            res.status(200).json({ access_token, password, email, id });
-        }
+    if (userInfo) {
+      const { email, password, id } = userInfo;
+      const bytes = utf8.encode(password);
+      const encoded = base64.encode(bytes);
+      const access_token = createToken({ email, password });
+      res.status(200).json({ access_token, password: encoded, email, id });
     }
+  }
+});
+// get user
+server.get("/users", (req, res) => {
+  const user = userdb.users;
+  if (user) {
+    res.jsonp(user);
+  }
 });
 
 // /auth/register
 server.post("/auth/register", (req, res) => {
-    const { email, password } = req.body;
-    if (isAuthenticated({ email, password })) {
-        const status = 401;
-        const message = "Tai khoan da ton tai";
-        res.status(status).json({ status, message });
-        return;
-    } else {
-        // get userID
-        const userInfo = {
-            id: faker.datatype.uuid(),
-            email: email,
-            password: password,
-            name: email,
-        };
+  const { email, password } = req.body;
+  if (isAuthenticated({ email, password })) {
+    const status = 401;
+    const message = "Tai khoan da ton tai";
+    res.status(status).json({ status, message });
+    return;
+  } else {
+    // get userID
+    const userInfo = {
+      id: faker.datatype.uuid(),
+      email: email,
+      password: password,
+      name: email,
+    };
 
-        const userdb = JSON.parse(fs.readFileSync("./db.json", "UTF-8"));
+    const userdb = JSON.parse(fs.readFileSync("./db.json", "UTF-8"));
 
-        const db = {
-            restaurants: [...userdb.restaurants],
-            users: [...userdb.users, userInfo],
-        };
-        fs.writeFile("./db.json", JSON.stringify(db), () => {
-            console.log("Successfully :))");
-        });
+    const db = {
+      restaurants: [...userdb.restaurants],
+      users: [...userdb.users, userInfo],
+    };
+    fs.writeFile("./db.json", JSON.stringify(db), () => {
+      console.log("Successfully :))");
+    });
 
-        const status = 200;
-        const message = "Dang ky thanh cong";
-        res.status(200).json({ status, message });
-    }
+    const status = 200;
+    const message = "Dang ky thanh cong";
+    res.status(200).json({ status, message });
+  }
 });
 
 // server.use(/^(?!\/auth).*$/, (req, res, next) => {
@@ -118,43 +129,43 @@ server.post("/auth/register", (req, res) => {
 // You can use the one used by JSON Server
 
 server.use((req, res, next) => {
-    if (req.method === "POST") {
-        req.body.createdAt = Date.now();
-        req.body.updatedAt = Date.now();
-    }
-    if (req.method === "PATCH") {
-        req.body.updatedAt = Date.now();
-    }
-    // Continue to JSON Server router
-    next();
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  }
+  if (req.method === "PATCH") {
+    req.body.updatedAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
 });
 
 router.render = (req, res) => {
-    // Check GET with pagination
-    // If yes, custom output
-    const headers = res.getHeaders();
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders();
 
-    const totalCountHeader = headers["x-total-count"];
-    if (req.method === "GET" && totalCountHeader) {
-        const queryParams = queryString.parse(req._parsedUrl.query);
+  const totalCountHeader = headers["x-total-count"];
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
 
-        const result = {
-            data: res.locals.data,
-            pagination: {
-                _page: Number.parseInt(queryParams._page) || 1,
-                _limit: Number.parseInt(queryParams._limit) || 5,
-                _totalRows: Number.parseInt(totalCountHeader),
-            },
-        };
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 5,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
 
-        return res.jsonp(result);
-    }
-    // otherwise
-    res.jsonp(res.locals.data);
+    return res.jsonp(result);
+  }
+  // otherwise
+  res.jsonp(res.locals.data);
 };
 
 server.use(middlewares);
 server.use(router);
 server.listen(PORT, () => {
-    console.log(`JSON Server is running at port : ${PORT}`);
+  console.log(`JSON Server is running at port : ${PORT}`);
 });
--- readme.md
+++ readme.md
@@ -4,12 +4,12 @@ You can run:
 
 # yarn start
 
--   Main.js:
-    Cấu hình router cho json server.
-    Tự động thêm createdAt và updatedAt vào trong req.body
--   random.js
-    Dùng "@faker-js/faker" để random dữ liệu
-    Dùng fs system built in nodejs để đọc và ghi file vào trong db.json
+- Main.js:
+  Cấu hình router cho json server.
+  Tự động thêm createdAt và updatedAt vào trong req.body
+- random.js
+  Dùng "@faker-js/faker" để random dữ liệu
+  Dùng fs system built in nodejs để đọc và ghi file vào trong db.json
 
 # Có hỗ trợ filter, sort, paginate
 
@@ -20,3 +20,7 @@ xem json server docs
 # /auth/login
 
 # /auth/register
+
+get user
+
+# /users
